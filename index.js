03605759c0adc17874bdca4600fb9f14d53e94e1
Dev Daksan <dev.91096@gmail.com>
2020-04-01 08:58:25 +0530

    Added morgan and dynamic route

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.use(morgan('tiny'));
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.get('/', (req, res) => res.send('Try movie/imdb_id/ to use the API'));
+
+app.get('/movie/:imdb_id', (req, res) => {
+  res.send(req.params.imdb_id);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
